849ee679e245f81d80bb6f201caa52a436bbf1ef
MSI <gsjksab>
2024-11-29 20:00:27 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+    #Program Manajemen Kontak
+
+
+# Program pilihan 1
+# menggunakan dictionarry
+kontak1 = {'Nama' : 'Andi', 'No.HP' : '08426127190', 'Email' : 'Andry@gmail.com'}
+kontak2 = {'Nama' : 'Bayu', 'No.HP' : '08426127188', 'Email' : 'Lukman@gmail.com'}
+# bentuk list
+kontak = [kontak1,kontak2]
+
+
+
+while True:
+    print("\n Menu Kontak")
+    print("1. Melihat Semua Kontak")
+    print("2. Menambahkan kontak baru")
+    print("3. Menghapus Kontak")
+    print("4. Keluar dari Kontak")
+
+    pilihan = input("Masukan pilihan menu kontak (1,2,3 atau 4): ")
+
+    if pilihan == '1':
+        # Melihat semua kontak
+        if kontak: # untuk memberitahu user bahwa kontak masih kosong, jika ada sisnya akan membentuk True tapi jika tidak ada nilainya akan menjadi False
+            for a,b in enumerate(kontak, start=1):
+                # variabel a di gunakan untuk menjadi angka urutannya
+                # variabel b untuk memanggil key dari dictionary
+                print(f"{a}. {b['Nama']} ({b['No.HP']} {b['Email']})")
+        else:
+            print("Kontak masih kosong!")
+
+    elif pilihan == '2':
+        # Menambahakan kontak baru
+        nama = input("Masukan Nama : ")
+        nohp = input("Masukan kontak : ")
+        email = input("masukan email : ")
+        # karena menggunakan konsep dictionary jadi dibuat bentuk dictionary
+        # mendiskripsikan variabel baru dengan variabel lama (Key di dictory yg di buat diatas) agar dapat di ubah
+        kontak_baru = {'Nama': nama, 'No.HP': nohp, 'Email': email}
+        # karna menambah list jadi menggunakan append
+        kontak.append(kontak_baru)
+        print("Kontak baru berhasil di tambahkan")
+
+    elif pilihan == '3':
+        # Menghapus Kontak
+        print("\n")
+        # Melihat semua kontak
+        if kontak:  # untuk memberitahu user bahwa kontak masih kosong, jika ada sisnya akan membentuk True tapi jika tidak ada nilainya akan menjadi False
+            for a, b in enumerate(kontak, start=1):
+                # variabel a di gunakan untuk menjadi angka urutannya
+                # variabel b untuk memanggil key dari dictionary
+                print(f"{a}. {b['Nama']} ({b['No.HP']} {b['Email']})")
+        else:
+            print("Kontak masih kosong!")
+            continue # agar terus berlanjut mengulang dari atas jika kontaknya kosong1
+
+        # membuat inputan angka untuk menghapus daftar nomor
+        # dan menginput angka harus menggunakan tipe data int atau float
+        # Layernya tidak di bawah for atau if
+        i_hapus = int(input("Masukan nomor yang akan di hapus : "))
+            # del di gunakan untuk menghapus variabel, objek maupun di dalam list
+            # kenapa (-1) karna datanya di mulai atau di start dari 1
+        del kontak[i_hapus-1]
+        print("Kontak Berhasil Di Hapus")
+
+    elif pilihan == '4':
+        # Keluar dari kontak
+        break
+    else:
+        print("Anda memasukkan pilihan yang salah!")
